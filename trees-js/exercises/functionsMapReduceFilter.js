1f4df7e27ceb0c8b34293893f40b98e413a141c5
ivekhov <ivekhov@gmail.com>
2022-12-24 18:50:06 +0300

    add exercise on trees - functions

--- trees-js/exercises/functionsMapReduceFilter.js
+++ trees-js/exercises/functionsMapReduceFilter.js
@@ -1,31 +1,65 @@
-/*
-https://ru.hexlet.io/challenges/js_trees_map_filter_reduce_exercise
-ToDo FILTER function
+// https://ru.hexlet.io/challenges/js_trees_map_filter_reduce_exercise
+
+import _ from 'lodash';
+import { mkdir, mkfile, getName, isDirectory, getChildren, getMeta } from '@hexlet/immutable-fs-trees';
+
+const tree = mkdir('/', [
+    mkdir('etc', [
+      mkdir('nginx', [
+        mkdir('conf.d'),
+      ]),
+      mkdir('consul', [
+        mkfile('config.json'),
+      ]),
+      mkdir('TEST_DIR', [
+        mkdir('TEST_CONF.d'),
+      ]),
+    ]),
+    mkfile('hosts'),
+    mkdir('OUTER_DIR', [
+       mkdir('OUTER_CONF.d'),
+	 mkdir('OUTER_CONF_ANOTHER.d'),
+    	 mkfile('FILE4'),
+    ]),
+    mkfile('FILE2'),
+    mkfile('FILE3'),
+  ]);
 
 
-filter принимает в качестве параметров предикат и дерево, а возвращает отфильтрованное дерево по предикату.
+const filter = (inner, tree) => {
 
-reduce кроме основных параметров (функция-обработчик и дерево) принимает также начальное значение аккумулятора.
+  const crawler = (innerFunc, item, acc) => {
+    if (!innerFunc(item)) {
+      return;
+    }
+	
+    const name = getName(item);
+    const newMeta = _.cloneDeep(getMeta(item));
+    const children = getChildren(item);
 
-map принимает на вход функцию-обработчик и дерево, а возвращает отображенное дерево.
+    acc['name'] = name; 
+    acc['meta'] = newMeta;
+    acc['type'] = item.type;
+    acc['children'] = [];
 
-map(n => ({ ...n, name: getName(n).toUpperCase() }), tree);
-{
-  name: '/',
-  type: 'directory',
-  meta: {},
-  children: [
-    {
-      name: 'ETC',
-      type: 'directory',
-      meta: {},
-      children: [{ name: 'CONFIG.JSON', type: 'file', meta: {} }],
-    },
-  ],
-}
- */
+    // ToDo - ? 
+    const newChildren = children.map((child) => crawler(innerFunc, child, acc));
+    acc['children'].push(newChildren);
 
-// map принимает на вход функцию-обработчик и дерево, а возвращает отображенное дерево.
+    return acc;
+  }
+
+  return crawler(inner, tree, {});
+};
+
+console.log(JSON.stringify(tree));
+console.log('---------------TEST------------------------------------');
+const res = filter((n) => isDirectory(n), tree);
+console.log(JSON.stringify(res));
+
+
+//----------------------------------
+
 const map = (inner, tree) => {
   const newTree = {};
   const crawler = (func, item, acc) => {
@@ -49,87 +83,6 @@ const map = (inner, tree) => {
   
 };
 
+//const test = map(n => ({ ...n, name: getName(n).toUpperCase() }), tree);
+//console.log(JSON.stringify(test));
 
-
-// Приводим имена всех директорий и файлов к верхнему регистру:
-// map(n => ({ ...n, name: getName(n).toUpperCase() }), tree);
-
-
-import { mkdir, mkfile, getName, isDirectory } from '@hexlet/immutable-fs-trees';
-
-const tree = mkdir('/', [
-  mkdir('eTc', [
-    mkfile('config.json')
-  ]),
-]);
-const files = mkfile('cOnfIg.json');
-const test = map(n => ({ ...n, name: getName(n).toUpperCase() }), tree);
-// console.log(map(n => ({ ...n, name: getName(n).toUpperCase() }), tree));
-// console.log(JSON.stringify(test));
-
-const tree2 = mkdir('/', [
-  mkdir('eTc', [
-    mkdir('NgiNx'),
-    mkdir('CONSUL', [
-      mkfile('config.json'),
-    ]),
-  ]),
-  mkfile('hOsts'),
-]);
-
-const actual = map((n) => ({ ...n, name: n.name.toUpperCase() }), tree2);
-// console.log(tree2);
-// console.log(JSON.stringify(actual));
-
-// filter принимает в качестве параметров предикат и дерево, а возвращает отфильтрованное дерево по предикату.
-const filter = (inner, tree) => {
-  const crawler = (innerFunc, item, acc) => {
-    if (innerFunc(item)) {
-      // console.log(item.name);
-      const entries = Object.entries(item);
-      for (const [key, value] of entries) {
-        if (key !== 'children') {
-          acc[key] = value;
-        } else {
-          acc['children'] = [];
-          const newAcc = {};
-          acc['children'].push(newAcc);
-          for (const element of item.children) {
-            if (isDirectory(element)) {
-              console.log(element);
-            }
-            crawler(innerFunc, element, newAcc);
-            // console.log(newAcc);
-            
-            // if (Object.keys(newAcc) < 1) {
-            //   acc['children'].pop();
-            // }
-            
-          }
-        }
-      }
-    }
-    return acc;
-  }
-  return crawler(inner, tree, {});
-};
-
-
-// console.log(tree);
-// const res = filter((n) => isDirectory(n), tree);
-// console.log(JSON.stringify(res));
-
-const treeF = mkdir('/', [
-  mkdir('etc', [
-    mkdir('nginx', [
-      mkdir('conf.d'),
-    ]),
-    mkdir('consul', [
-      mkfile('config.json'),
-    ]),
-  ]),
-  mkfile('hosts'),
-]);
-
-const resF = filter((n) => isDirectory(n), treeF);
-console.log(JSON.stringify(resF));
