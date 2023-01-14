848c237fc862bb89e4ac558a254c7ea2d7559e22
ivekhov <ivekhov@gmail.com>
2023-01-14 21:39:50 +0300

    update dom exercises

--- browser-dom/dom.js
+++ browser-dom/dom.js
@@ -96,3 +96,115 @@ export default (document) => {
   // END
 
  * */
+
+
+const prettify =  (document) => {
+  const items = document.querySelectorAll('div');
+  const elems = Array.from(items);
+  
+  elems.map((elem) => {
+    Array.from(elem.childNodes).map((child) => {
+      
+      if (child instanceof Text) {
+        const input = child.textContent.trim();
+        if(input !== '') {
+            const p = document.createElement('p');
+            p.textContent = child.textContent.trim();
+            child.replaceWith(p); 
+          }
+        }
+      });
+  });
+};
+
+/*
+export default (document) => {
+  const divs = [...document.getElementsByTagName('div')];
+  divs.forEach((div) => {
+    const textNodes = [...div.childNodes]
+      .filter((child) => child instanceof Text)
+      .filter((child) => child.textContent.trim() !== '');
+    textNodes.forEach((node) => {
+      const p = document.createElement('p');
+      p.textContent = node.textContent;
+      node.replaceWith(p);
+    });
+  });
+};
+ * */
+
+// classNames to camelCase
+const converToCamelCase = (document) => {
+  const selectorsArr = [...document.body.querySelectorAll('*')];
+  selectorsArr.map((item) => {
+    item.className = item
+      .className.toString().split(' ').map(i => camelCase(i)).join(' ');
+  });
+};
+/* ATTEMPTS: 
+
+
+get elems in doc - all tags?
+
+get arr of tags:
+const tagsArr = [...document.body.getElementsByTagName('*')];
+tagsArr[1]
+?
+
+
+const selectorsRaw = document.body.querySelectorAll('*');
+selectors.forEach()
+not works:
+selectorsRaw.replace('a', 'AAAAAA')
+
+getClassList ?
+
+?
+document.body.classList
+
+?
+document.body.querySelector("div");
+
+not work
+[...document.querySelectorAll('*')].map(i => [...i.classList]);
+document.querySelectorAll('*').map(i => [...i.classList]);
+
+selectorsArr.map)()
+selectorsArr.map(i => [...i.classList])
+
+selectorsArr[2].classList.value
+
+
+[...new Set([...document.querySelectorAll('*')].map(i=>[...i.classList]).flat(Infinity).sort())];
+
+const span = document.querySelector("span");
+const classes = span.classList;
+const result = classes.replace("c", "z"); 
+
+QUESTIONS: 
+1 разница между  getElementsByTagName и  getElementsByTagName ?
+2 почему document.body.classList не отдает список классов в теле ?
+*/
+
+/*
+TEACHERS VERSION:
+export default (document) => {
+  const allNodes = [...document.body.getElementsByTagName('*')];
+  allNodes.forEach((node) => {
+    const process = (item) => node.classList.replace(item, camelCase(item));
+    node.classList.forEach(process);
+  });
+};
+*/
+
+/*
+- обращаться через document.body.
+- getElementsByTagName('*') - все объекты и их передавать в массив для последующего обхода.
+- обход коллекции (Массива) через метод forEach и изменении объекта
+- элемент при обходе коллекции может отдавать classList,
+- в свою очередь: у каждого такого списка классов может вызываться метод forEach
+
+*/
+
+
+
