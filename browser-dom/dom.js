f8aa1a150682cc19b4d29c0e7ebe23730cd09da4
ivekhov <ivekhov@gmail.com>
2023-01-17 23:30:30 +0300

    add exercise on dom

--- browser-dom/dom.js
+++ browser-dom/dom.js
@@ -228,3 +228,23 @@ export default () => {
   // END
 };
 
+
+/*
+// BEGIN
+  const button = document.getElementById('alert-generator');
+  const alertsBox = document.querySelector('.alerts');
+  let i = 1;
+  button.addEventListener('click', () => {
+    const alert = document.createElement('div');
+    alert.classList.add('alert', 'alert-primary');
+    alert.textContent = `Alert ${i}`;
+
+    alertsBox.prepend(alert);
+
+    i += 1;
+  });
+  // END
+ */
+
+
+
